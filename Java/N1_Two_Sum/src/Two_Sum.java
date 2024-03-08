34cdf215bd2816a94d9bf32a6b96cb79ab0fdb84
Zvolvium <motschke61@gmail.com>
2024-03-08 17:32:28 -0600

    Adding Two_Sum.java and IntelliJ project files

---
+++ Java/N1_Two_Sum/src/Two_Sum.java
@@ -0,0 +1,28 @@
+/*
+    Title: Two Sum
+
+    Difficulty: Easy
+
+    https://leetcode.com/problems/two-sum/description/
+
+    Java Version: JDK-21
+
+    Problem Description:
+
+        Given an array of integers nums and an integer target,
+            -> return indices of the two numbers such that they add up to target.
+
+        You may assume that each input would have exactly one solution,
+            -> and you may not use the same element twice.
+
+        You can return the answer in any order.
+
+        Follow-up:
+            Can you come up with an algorithm that is less than O(n^2) time complexity?
+*/
+
+public class Two_Sum {
+    public static void main (String[] args){
+        System.out.println("Hello World!!");
+    }
+}
